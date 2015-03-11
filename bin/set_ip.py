674f32a3791ec419ec9875572565e9e6a7d29f04
Hua Yanghao <yanghao.hua@intel.com>
2015-03-11 22:54:43 +0800

    initial commit

---
+++ bin/set_ip.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from httplib import HTTPConnection
+place = sys.argv[1]
+password = sys.argv[2]
+server = "go.xiaoyezi.org"
+
+conn = HTTPConnection(server, 80)
+
+conn.request("GET", "/ip")
+response = conn.getresponse()
+if response.status == 200:
+    ip = response.read()
+    conn.request("GET", "/ipset/%s/%s/%s" % (place, ip, password))
+    r = conn.getresponse()
+    print(r.read())
